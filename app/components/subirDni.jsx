1cb1e6c37c2172239826646f7052d29115e0ef86
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2024-04-16 12:28:56 -0300

    arreglo responsive

--- app/components/subirDni.jsx
+++ app/components/subirDni.jsx
@@ -1,13 +1,11 @@
-'use client'
+"use client";
 import Image from "next/image";
 import React from "react";
 import { ImUpload3 } from "react-icons/im";
 import { FaInfoCircle } from "react-icons/fa";
 import Swal from "sweetalert2";
 
-
 const SubirDni = () => {
-
   const handleSweetAlertDni = () => {
     Swal.fire({
       title: "Agregar ejemplo",
@@ -20,7 +18,7 @@ const SubirDni = () => {
         <div className="flex flex-col border rounded-xl py-10 px-12 gap-6 w-full md:w-1/2">
           <div className="flex justify-center items-center">
             <button className="bg-blue-600 text-white w-2/3 py-2 rounded-lg font-semibold flex items-center justify-center gap-3">
-            <p className="flex justify-center items-center text-xl">
+              <p className="flex justify-center items-center text-xl">
                 <ImUpload3 />
               </p>
               Frente DNI
@@ -54,19 +52,22 @@ const SubirDni = () => {
           </div>
         </div>
       </div>
-      <div className="flex ">
-      <p className="ml-5 p-7 text-left font-semibold"> Especificaciones para la validación de su DNI</p>
-      <button
-              className="h-12 w-12 rounded-full"
-              onClick={handleSweetAlertDni}
-            >
-              <p className="text-blue-700 text-4xl hover:scale-125  transition duration-300">
-                <FaInfoCircle />
-              </p>
-            </button>
+      <div className="flex justify-start items-center">
+        <p className="ml-5 p-7 text-left font-semibold">
+          {" "}
+          Especificaciones para la validación de su DNI
+        </p>
+        <button
+          className="h-12 w-12 rounded-full"
+          onClick={handleSweetAlertDni}
+        >
+          <p className="text-blue-700 text-4xl hover:scale-125  transition duration-300">
+            <FaInfoCircle />
+          </p>
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SubirDni;
